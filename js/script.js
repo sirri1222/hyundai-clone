110cb37691aa00aec35a6fd8f34e92723161e918
sirri1222 <hyeonji159753@gmail.com>
2023-04-04 18:36:38 +0900

    마무리 작업 진행중

--- js/script.js
+++ js/script.js
@@ -1,4 +1,109 @@
 window.onload = function () {
+  AOS.init();
+
+  // ** FADE OUT FUNCTION **
+  // fadeOut( element : document.querySelctor(대상) )
+  function fadeOut(el) {
+    // 대상.style.투명도 = 불투명
+    el.style.opacity = 1;
+    (function fade() {
+      // 대상.style.투명도 -= 0.1 감소
+      if ((el.style.opacity -= 0.1) < 0) {
+        el.style.display = "none";
+      } else {
+        // 웹브라우저 프레임갱신
+        requestAnimationFrame(fade);
+      }
+    })();
+  }
+
+  // ** FADE IN FUNCTION **
+  // fadeIn( element : document.querySelctor(대상) )
+  function fadeIn(el, display) {
+    el.style.opacity = 0;
+    el.style.display = display || "block";
+    (function fade() {
+      var val = parseFloat(el.style.opacity);
+      if (!((val += 0.1) > 1)) {
+        el.style.opacity = val;
+        requestAnimationFrame(fade);
+      }
+    })();
+  }
+
+  // 모달창
+  let body = document.querySelector("body");
+  let modal = document.querySelector(".modal");
+  modal.addEventListener("click", function () {
+    // modal.style.display = "none";
+    // fadeOut(modal);
+    anime({
+      targets: ".modal",
+      delay: 200,
+      duration: 500,
+      opacity: 0,
+      easing: "easeInOutQuad",
+      complete: function () {
+        modal.style.display = "none";
+        body.classList.add("active");
+      },
+    });
+  });
+  // 스크롤 기능
+  // 스크롤바의 상단위치
+  let scy = 0;
+  let scActive = 50;
+  scy = window.document.documentElement.scrollTop;
+
+  let header = document.querySelector(".header");
+  let logoW = document.querySelector(".logo-w");
+  let logoG = document.querySelector(".logo-g");
+
+  header.addEventListener("mouseenter", function () {
+    header.classList.add("header-active");
+    logoW.style.display = "none";
+    logoG.style.display = "block";
+  });
+  header.addEventListener("mouseleave", function () {
+    if (scy < scActive) {
+      header.classList.remove("header-active");
+      logoW.style.display = "block";
+      logoG.style.display = "none";
+    }
+  });
+
+  // 새로고침 시
+  if (scy > scActive) {
+    header.classList.add("header-active");
+    logoW.style.display = "none";
+    logoG.style.display = "block";
+  }
+
+  window.addEventListener("scroll", function () {
+    scy = window.document.documentElement.scrollTop;
+    // console.log("스크롤 : " + scy);
+    if (scy > scActive) {
+      header.classList.add("header-active");
+      logoW.style.display = "none";
+      logoG.style.display = "block";
+    } else {
+      header.classList.remove("header-active");
+      logoW.style.display = "block";
+      logoG.style.display = "none";
+    }
+  });
+
+  // 펼침 언어 기능
+  const langWord = document.querySelector(".language-word");
+  const language = document.querySelector(".language");
+  const languageLi = document.querySelector(".language li");
+  setTimeout(function () {
+    languageLi.style.transition = "all 0.2s";
+  }, 500);
+
+  langWord.addEventListener("click", function () {
+    language.classList.toggle("language-box-active");
+  });
   // 메뉴기능
   const nav = document.querySelector(".nav");
   const btMenu = document.querySelector(".bt-menu");
@@ -51,7 +156,13 @@ window.onload = function () {
     // 다음 비디오 재생
     // 처음으로 비디오 플레이헤드 이동
     videos[videoIndex].currentTime = 0;
-    videos[videoIndex].play();
+
+    // https://solbel.tistory.com/1912
+    // videos[videoIndex].play();
+    const playPromise = videos[videoIndex].play();
+    if (playPromise !== undefined) {
+      playPromise.then((_) => {}).catch((error) => {});
+    }
 
     // 방어코드: 다음주 추가 설명
     clearInterval(videoTimer);
@@ -118,5 +229,15 @@ window.onload = function () {
       // 슬라이드명.slidTo(번호)
       swVisual.slideTo(videoIndex);
     });
+  });
+
+  // 비즈니스 슬라이드
+  const swBusiness = new Swiper(".swBusiness", {
+    loop: true,
+    speed: 500,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
   });
 };
